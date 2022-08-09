a086a608efe645726c69f39bc86596f778da3e8a
JovitaBK <99868147+JovitaBK@users.noreply.github.com>
2022-08-09 19:37:49 +0300

    pridetas dar vienas skaicius

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def suskaiciuoti_suma (sk1, sk2, sk3):
-    rezultatas = sk1 +sk2 +sk3
+def suskaiciuoti_suma (sk1, sk2, sk3, sk4):
+    rezultatas = sk1 +sk2 +sk3 +sk4
     print(rezultatas)
 
-suskaiciuoti_suma(1,2,3)
+suskaiciuoti_suma(1,2,3,4)
+

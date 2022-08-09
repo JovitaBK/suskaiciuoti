143af731356b8b8adc25d74ca5d72787ca4d190a
JovitaBK <99868147+JovitaBK@users.noreply.github.com>
2022-08-09 19:50:37 +0300

    i antra funkcija irasom neigiama skaiciu

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def suskaiciuoti_suma (sk1, sk2, sk3, sk4):
 
 suskaiciuoti_suma(1,2,3,4)
 
-def suskaiciuoti_skirtuma(sk1, sk2, sk3):
-    skirtumo_rezultatas = (sk1 - sk2 - sk3)
+def suskaiciuoti_skirtuma(sk1, sk2, sk3, sk4):
+    skirtumo_rezultatas = (sk1 - sk2 - sk3 - sk4)
     print(skirtumo_rezultatas)
 
-suskaiciuoti_skirtuma(5, 3,1)
+suskaiciuoti_skirtuma(5, 3,1, -1)

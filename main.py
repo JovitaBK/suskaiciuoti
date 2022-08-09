4f42e83b89837ffa50e26a2b967e5dbd5494b54e
JovitaBK <99868147+JovitaBK@users.noreply.github.com>
2022-08-09 19:48:33 +0300

    prideta dar viena funkcija

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def suskaiciuoti_suma (sk1, sk2, sk3, sk4):
 
 suskaiciuoti_suma(1,2,3,4)
 
+def suskaiciuoti_skirtuma(sk1, sk2, sk3):
+    skirtumo_rezultatas = (sk1 - sk2 - sk3)
+    print(skirtumo_rezultatas)
+
+suskaiciuoti_skirtuma(5, 3,1)
